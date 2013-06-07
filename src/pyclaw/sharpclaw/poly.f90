a2a5da46f1e4879c6b236e1f79f6933eae3d8c5b
damiansra <damiansra@gmail.com>
2013-06-07 17:47:17 +0300

    flatten poly

--- src/pyclaw/sharpclaw/poly.f90
+++ src/pyclaw/sharpclaw/poly.f90
@@ -12,7 +12,7 @@ contains
         double precision, intent(out) :: ql(num_eqn,maxnx+2*num_ghost),qr(num_eqn,maxnx+2*num_ghost)
 
         integer :: i,n
-
+        
         do n = 1,num_eqn
             do i = num_ghost,maxnx+num_ghost+1
 
@@ -35,14 +35,14 @@ contains
         double precision, intent(out) :: ql(num_eqn,maxnx+2*num_ghost),qr(num_eqn,maxnx+2*num_ghost)
 
         integer :: i,n
-
+        
         do n = 1,num_eqn
             do i = num_ghost,maxnx+num_ghost+1
 
-                ql(n,i) = (7.d0*(q(n,-3+i) - 10.d0*q(n,-2+i) + 75.d0*q(n,-1+i) + 100.d0*q(n,i) &
-                           - 25.d0*q(n,1+i) + 6.d0*q(n,2+i)) - 5.d0*q(n,3+i))/1024.d0
-                qr(n,i) = (7.d0*(6.d0*q(n,-2+i) - 25.d0*q(n,-1+i) + 100.d0*q(n,i) &
-                           + 75.d0*q(n,1+i) - 10.d0*q(n,2+i) + q(n,3+i)) - 5.d0*q(n,-3+i))/1024.d0
+                ql(n,i) = (7.d0*q(n,-3+i) - 70.d0*q(n,-2+i) + 525.d0*q(n,-1+i) + 700.d0*q(n,i) &
+                           - 175.d0*q(n,1+i) + 42.d0*q(n,2+i) - 5.d0*q(n,3+i))/1024.d0
+                qr(n,i) = (42.d0*q(n,-2+i) - 175.d0*q(n,-1+i) + 700.d0*q(n,i) &
+                           + 525.d0*q(n,1+i) - 70.d0*q(n,2+i) + 7.d0*q(n,3+i) - 5.d0*q(n,-3+i))/1024.d0
 
             end do
         end do
@@ -60,14 +60,14 @@ contains
         double precision, intent(out) :: ql(num_eqn,maxnx+2*num_ghost),qr(num_eqn,maxnx+2*num_ghost)
 
         integer :: i,n
-
+        
         do n = 1,num_eqn
             do i = num_ghost,maxnx+num_ghost+1
-                        
-                ql(n,i) = (-45.d0*q(n,-4+i) + 42.d0*(12.d0*q(n,-3+i) + 7.d0*(-10*q(n,-2+i) + 60.d0*q(n,-1+i) &
-                           + 75.d0*q(n,i) - 20.d0*q(n,1+i) + 6.d0*q(n,2+i))) - 360.d0*q(n,3+i) + 35.d0*q(n,4+i))/32768.d0
-                qr(n,i) = (35.d0*q(n,-4+i) - 360.d0*q(n,-3+i) + 294.d0*(6*q(n,-2+i) - 20.d0*q(n,-1+i) + 75.d0*q(n,i) &
-                           + 60.d0*q(n,1+i) - 10.d0*q(n,2+i)) + 504.d0*q(n,3+i) - 45.d0*q(n,4+i))/32768.d0
+                   
+                ql(n,i) = (-45.d0*q(n,-4+i) + 504.d0*q(n,-3+i) - 2940.d0*q(n,-2+i) + 17640.d0*q(n,-1+i) &
+                           + 22050.d0*q(n,i) - 5880.d0*q(n,1+i) + 1764.d0*q(n,2+i) - 360.d0*q(n,3+i) + 35.d0*q(n,4+i))/32768.d0
+                qr(n,i) = (35.d0*q(n,-4+i) - 360.d0*q(n,-3+i) + 1764.d0*q(n,-2+i) - 5880.d0*q(n,-1+i) + 22050.d0*q(n,i) &
+                           + 17640.d0*q(n,1+i) - 2940.d0*q(n,2+i) + 504.d0*q(n,3+i) - 45.d0*q(n,4+i))/32768.d0
 
             end do
         end do
@@ -88,13 +88,15 @@ contains
 
         do n = 1,num_eqn
             do i = num_ghost,maxnx+num_ghost+1
-
-                ql(n,i) = (77.d0*q(n,-5+i) - 99.d0*(10.d0*q(n,-4+i) - 7.d0*(9.d0*q(n,-3+i) - 40.d0*q(n,-2+i) &
-                           + 210.d0*q(n,-1+i) + 252.d0*q(n,i) - 70.d0*q(n,1+i) + 24.d0*q(n,2+i)) + 45.d0*q(n,3+i)) &
+!                ql(n,i) = (77.d0*q(n,-5+i) - 99.d0*(10.d0*q(n,-4+i) - 7.d0*(9.d0*q(n,-3+i) - 40.d0*q(n,-2+i) &
+!                           + 210.d0*q(n,-1+i) + 252.d0*q(n,i) - 70.d0*q(n,1+i) + 24.d0*q(n,2+i)) + 45.d0*q(n,3+i)) &
+!                           + 770.d0*q(n,4+i) - 63.d0*q(n,5+i))/262144.d0
+                ql(n,i) = (77.d0*q(n,-5+i) - 990.d0*q(n,-4+i) + 6237.d0*q(n,-3+i) - 27720.d0*q(n,-2+i) &
+                           + 145530.d0*q(n,-1+i) + 174636.d0*q(n,i) - 48510.d0*q(n,1+i) + 16632.d0*q(n,2+i) - 4455.d0*q(n,3+i) &
                            + 770.d0*q(n,4+i) - 63.d0*q(n,5+i))/262144.d0
-                qr(n,i) = (-63.d0*q(n,-5+i) + 11.d0*(70.d0*q(n,-4+i) - 405.d0*q(n,-3+i) + 63.d0*(24.d0*q(n,-2+i) & 
-                           - 70.d0*q(n,-1+i) + 252.d0*q(n,i) + 210.d0*q(n,1+i) - 40.d0*q(n,2+i) + 9.d0*q(n,3+i)) &
-                           - 90.d0*q(n,4+i) + 7.d0*q(n,5+i)))/262144.d0
+                qr(n,i) = (-63.d0*q(n,-5+i) + 770.d0*q(n,-4+i) - 4455.d0*q(n,-3+i) + 16632.d0*q(n,-2+i) & 
+                           - 48510.d0*q(n,-1+i) + 174636.d0*q(n,i) + 145530.d0*q(n,1+i) - 27720.d0*q(n,2+i) + 6237.d0*q(n,3+i) &
+                           - 990.d0*q(n,4+i) + 77.d0*q(n,5+i))/262144.d0
             end do
         end do
     end subroutine poly10
